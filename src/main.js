5b43dbfd0b59a4ab4e3729e095bad34f3518f95f
johnflynn <johnflynn493@gmail.com>
2021-08-15 20:00:21 -0400

    Added drawing enemy entity.

--- src/main.js
+++ src/main.js
@@ -3,6 +3,21 @@ var rand = require('./rand');
 var key = require('./key');
 var mouse = require('./mouse');
 
+// enemy class... todo move into another file and set up imports and stuff...
+class Enemy {
+
+  constructor(id, x, y) {
+    this.id = id
+    this.health = 100;
+    this.x = x
+    this.y = y
+    this.height = 60
+    this.width = 60
+  }
+
+}
+
+
 // Debounce for player firing cannon.
 const PROJECTILE_DBOUNCE_SEC = 0.20 // 1 20th of a second.
 
@@ -14,10 +29,19 @@ document.body.appendChild(canvas);
 
 var ctx = canvas.getContext('2d');
 
+// Load player.
 let loaded = false
 let image = new Image()
-image.src = "ship.png"
+image.src = "assets/ship.png"
 image.onload = () => { loaded = true }
+
+
+// Load enemy..
+let enemyAssetLoaded = false
+let enemyImage = new Image()
+enemyImage.src = "assets/enemy.png"
+enemyImage.onload = () => { enemyAssetLoaded = true }
+
 
 /**
  * contains player object and list of all projectiles on board.
@@ -26,14 +50,17 @@ const gameState = {
   player: {
     x: rand.int(canvas.width),
     y: rand.int(canvas.height),
-    width: 50,
-    height: 50,
+    width: 60,
+    height: 60,
     speed: 250,
     color: 'rgba(236, 94, 103, 1)',
     projectileFiredSince: PROJECTILE_DBOUNCE_SEC,
+    health: 100,
   },
-  projectiles: [],
+  level: 1,
+  playerProjectiles: [],
   enemies: [],
+  enemyProjectiles: [],
 }
 
 const isProjectileFireable = (dt, firedSince) => (firedSince) >= PROJECTILE_DBOUNCE_SEC
@@ -43,7 +70,7 @@ const isProjectileFireable = (dt, firedSince) => (firedSince) >= PROJECTILE_DBOU
  * @param gameState
  */
 const doFireProjectile = (gameState) => {
-  gameState.projectiles.push({
+  gameState.playerProjectiles.push({
     x: (gameState.player.x + (gameState.player.height / 2)),
     y: gameState.player.y,
     width: 5,
@@ -96,14 +123,26 @@ loop.start(function (dt) {
     ctx.drawImage(image, gameState.player.x, gameState.player.y, gameState.player.width, gameState.player.height)
   }
 
-  // draw gameState.projectiles.
-  gameState.projectiles.forEach((projectile, idx) => {
+  // draw enemies...
+  if (enemyAssetLoaded) {
+    console.log('drawing enemies...')
+    gameState.enemies.forEach((enemy) =>
+    ctx.drawImage(enemyImage, enemy.x, enemy.y, enemy.width, enemy.height))
+  }
+
+  // fudge enemy into existance.
+  if (gameState.enemies.length === 0) {
+    gameState.enemies.push(new Enemy(gameState.enemies.length, rand.int(canvas.width), rand.int(canvas.height)))
+  }
+
+  // draw gameState.playerProjectiles.
+  gameState.playerProjectiles.forEach((projectile, idx) => {
     ctx.fillStyle = projectile.color;
     ctx.fillRect(projectile.x, projectile.y, projectile.width, projectile.height);
     // Move the projectile in 2d space todo add direction..
     // projectile.x = projectile.x - (projectile.speed * dt)
     projectile.y = projectile.y - (projectile.speed * dt)
-    gameState.projectiles[idx] = projectile
+    gameState.playerProjectiles[idx] = projectile
   })
 
   gameState.player.projectileFiredSince = gameState.player.projectileFiredSince + dt;
